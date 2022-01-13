205b602f552ff07c0228d8fca7a85fcf97167355
Alex20511998 <Alex-2504@ukr.net>
2022-01-13 13:51:35 +0200

    hw_1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  let [fligtsList, setFligtsList] = useState([]);
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/launches')
+         .then(value => value.json())
+         .then(flights => {
+            setFligtsList(flights.filter(flights => flights.launch_year !== '2020'));
+
+        });
+  },[])
   return (
-   <div><h1>Hello</h1></div>
+   fligtsList.map(value => <div key={value.flight_number}>
+     <div className={'fligts'}>
+         <div className={'fligtsLeft'}>
+             <h2>{value.mission_name}</h2>
+             <p>{value.launch_year}</p>
+         </div>
+         <div className={'fligtsRight'}>
+             <img src={value.links.mission_patch} alt="mission_patch"/>
+         </div>
+
+     </div>
+     
+   </div>)
   );
 }
 
