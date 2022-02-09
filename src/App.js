0cdf8822460e7b27452c460adc84fdd342174a90
Alex20511998 <Alex-2504@ukr.net>
2022-02-09 16:10:54 +0200

    cw_5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Form from "./Form/Form";
 import AddAction from "./AddAction/AddAction";
+import {useReducer} from "react";
 
 function App() {
-
+ [state, dispatch] = useReducer(reducer, addAction:{})
 
     return (
         <div>
